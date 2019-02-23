5064a579b6d97259d4460fb1defc0cc09de03856
David <khyu777@gmail.com>
2019-02-23 15:16:30 -0500

    delete extra copy and update map to include popup with graph

--- app.R
+++ app.R
@@ -1,17 +1,26 @@
 #load libraries
 library(shiny)
+library(shinyBS)
 library(tidyverse)
-library(plotly)
 library(leaflet)
 
 #set working directory to file source directory
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 #read in data and obtain aggregate
 df_orig = read_csv("data/test.csv") %>% 
-  janitor::clean_names() %>% 
-  select(country:co)
-df <- aggregate(. ~ country, df_orig, sum)
+  select(Country:CO) %>% 
+  rename("PM2.5" = pm25)
+df <- aggregate(. ~ Country, df_orig, sum)
+
+BD <- read_csv("data/bangladesh.csv") %>% 
+  mutate(country = recode(country, "BD" = "Bangladesh"))
+IN <- read_csv("data/india.csv") %>% 
+  mutate(country = recode(country, "IN" = "India"))
+TH <- read_csv("data/thailand.csv") %>% 
+  mutate(country = recode(country, "TH" = "Thailand"))
+pm <- do.call(rbind, list(BD, IN, TH)) %>% 
+  select(country, local, parameter, value)
 
 #Download .shp file on the web:
 #download.file("http://thematicmapping.org/downloads/TM_WORLD_BORDERS_SIMPL-0.3.zip", destfile = "world_shape_file.zip")
@@ -22,20 +31,27 @@ library(rgdal)
 world_spdf=readOGR(dsn = getwd(), layer="TM_WORLD_BORDERS_SIMPL-0.3")
 
 #add in our data to shapefile
-tmp <- left_join(world_spdf@data, df, by = c("NAME" = "country")) %>% 
-  select(NAME, LON:co)
+tmp <- left_join(world_spdf@data, df, by = c("NAME" = "Country")) %>% 
+  select(NAME, LON:CO) 
+tmp <- tmp %>% 
+  mutate(id=1:nrow(tmp))
 world_spdf@data <- tmp
 
 #create dataframe in a tidy format
-tidy_world <- tmp %>% 
-  gather(pollutant, num_sources, pm25:co)
+tidy_world <- tmp %>%
+  gather(pollutant, num_sources, PM2.5:CO)
 
 #shiny stuff
 ui <- fluidPage(
-  selectInput(inputId = "pollutant", label = "Choose a pollutant", choices = unique(df_tidy$pollutant)),
-  
+  fluidRow(
+    column(2,
+           selectInput(inputId = "pollutant", label = "Choose a pollutant", choices = unique(tidy_world$pollutant))
+           ),
+    column(10,
+           leafletOutput("mymap", height = "85vh")
+           )
+  )
   #plotOutput("plot"),
-  leafletOutput("mymap", height = "85vh")
 )
 
 server <- function(input, output) {
@@ -55,44 +71,93 @@ server <- function(input, output) {
   
   output$mymap <- renderLeaflet({
     
+    #create leaflet map
+    leaflet(world_spdf) %>% 
+      addProviderTiles("Esri.WorldGrayCanvas") %>% 
+      setView(lat = 20, lng = 95, zoom = 4) 
+  })
+  
+  bins <- c(0,2,4,6,8,10)
+  
+  observe({
     #set bin and color category
-    bins <- c(0,2,4,6,8,10)
     pal <- colorBin("YlOrRd", domain = df_subset()$num_sources, bins = bins, na.color = "transparent")
     
     #set text popup
     mytext=paste("Country: ", df_subset()$NAME,"<br/>", "# of Sources: ", df_subset()$num_sources) %>%
       lapply(htmltools::HTML)
     
-    #create leaflet map
-    leaflet(world_spdf) %>% 
-      addProviderTiles("Esri.WorldGrayCanvas") %>% 
-      setView(lat = 20, lng = 95, zoom = 4) %>% 
-      addPolygons(
-        fillColor = ~pal(df_subset()$num_sources),
-        weight = 2,
-        opacity = 1,
-        color = "black",
-        dashArray = "",
-        fillOpacity = .6,
-        highlight = highlightOptions(
-          weight = 5,
-          color = "black",
-          dashArray = "",
-          fillOpacity = .6,
-          bringToFront = TRUE),
-        label = mytext,
-        labelOptions = labelOptions(
-          style = list("font-weight" = "normal", padding = "3px 8px"),
-          textsize = "15px",
-          direction = "auto")
-      ) %>% 
+    #add data
+    leafletProxy("mymap", data = world_spdf) %>%
+                   clearShapes() %>% 
+                   addPolygons(
+                     fillColor = ~pal(df_subset()$num_sources),
+                     weight = 1.5,
+                     opacity = 1,
+                     color = "grey",
+                     dashArray = "",
+                     fillOpacity = .7,
+                     layerId = df_subset()$id,
+                     highlight = highlightOptions(
+                       weight = 5,
+                       color = "black",
+                       dashArray = "",
+                       fillOpacity = .7,
+                       bringToFront = TRUE),
+                     label = mytext,
+                     labelOptions = labelOptions(
+                       style = list("font-weight" = "normal", padding = "3px 8px"),
+                       textsize = "15px",
+                       direction = "auto")
+                     )
+    })
+  
+  #create legend
+  observe({
+    proxy <- leafletProxy("mymap", data = df_subset())
+    proxy %>% clearControls()
+    proxy %>% 
       addLegend(
-        pal = pal, 
-        values = ~world_spdf@data$ozone, 
+        pal <- colorBin("YlOrRd", domain = df_subset()$num_sources, bins = bins, na.color = "transparent"), 
+        values = ~df_subset()$num_sources, 
         opacity = 0.7, 
         title = "Number of sources", 
         position = "bottomright"
       )
+  })
+  
+  rv <- reactiveValues()
+  #output plot based on click
+  observeEvent(input$mymap_shape_click, {
+    event <- input$mymap_shape_click
+    name <- df_subset()$NAME[df_subset()$id == event$id]
+    if (name %in% pm$country){
+      rv$tb <- pm %>% 
+        filter(country == name) %>% 
+        select(country, parameter, value, local)
+      output$plot <- renderPlot({
+        rv$tb %>% ggplot() +
+          geom_point(aes(local, value))
+      })
+      showModal(
+        modalDialog(
+          title = paste(name, " ", input$pollutant),
+          plotOutput("plot"),
+          footer = actionButton("dismiss_modal",label = "Dismiss")
+        )
+      )
+    } else {
+      showModal(
+        modalDialog(
+          "No data avilable"
+        )
+      )
+    }
+  })
+  
+  observeEvent(input$dismiss_modal, {
+    output$plot <- NULL
+    removeModal()
   })
 }
 
