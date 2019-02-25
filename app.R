9ccbe56b289de9e73e6b2fdfa3524767f3711a1b
David <khyu777@gmail.com>
2019-02-25 16:47:22 -0500

    change from continuous to categorical output and add reset view button

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 #check for missing packages and install
 list.of.packages <- c("shiny", "shinydashboard", "tidyverse", "leaflet", "rgdal")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
-if(length(new.packages)) install.packages(new.packages)
+if (length(new.packages)) install.packages(new.packages)
 
 #load libraries
 library(shiny)
@@ -12,21 +12,22 @@ library(leaflet)
 #set working directory to file source directory
 #setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-#read in data and obtain aggregate
-df_orig = read_csv("data/test.csv") %>% 
+#read in availability data and obtain aggregate
+df_orig <- read_csv("data/test.csv") %>% 
   select(Country:CO) %>% 
   rename("PM2.5" = pm25)
 df <- aggregate(. ~ Country, df_orig, sum)
 
+#combine actual pollutant data
 BD <- read_csv("data/bangladesh.csv") %>% 
-  mutate(country = recode(country, "BD" = "Bangladesh"), value = replace(value, which(value<0), NA))
+  mutate(country = recode(country, "BD" = "Bangladesh"), value = replace(value, which(value < 0), NA))
 IN <- read_csv("data/india.csv") %>% 
   mutate(country = recode(country, "IN" = "India"))
 TH <- read_csv("data/thailand.csv") %>% 
   mutate(country = recode(country, "TH" = "Thailand"))
 pm <- do.call(rbind, list(BD, IN, TH)) %>% 
   select(country, local, parameter, value) %>% 
-  mutate(parameter = recode(parameter, "pm25" = "PM2.5", "o3" = "Ozone", "no2" = "NOx", "so2" = "SO2", "co" = "CO"  ))
+  mutate(parameter = recode(parameter, "pm25" = "PM2.5", "o3" = "Ozone", "no2" = "NOx", "so2" = "SO2", "co" = "CO"))
 
 #Download .shp file on the web:
 #download.file("http://thematicmapping.org/downloads/TM_WORLD_BORDERS_SIMPL-0.3.zip", destfile = "world_shape_file.zip")
@@ -34,7 +35,7 @@ pm <- do.call(rbind, list(BD, IN, TH)) %>%
 
 #Read the shp file with the rgdal library in R
 library(rgdal)
-world_spdf=readOGR(dsn = getwd(), layer="TM_WORLD_BORDERS_SIMPL-0.3")
+world_spdf <- readOGR(dsn = getwd(), layer = "TM_WORLD_BORDERS_SIMPL-0.3")
 
 #subset asia shp
 world_spdf <- subset(world_spdf, REGION == 142)
@@ -43,26 +44,28 @@ world_spdf <- subset(world_spdf, REGION == 142)
 tmp <- left_join(world_spdf@data, df, by = c("NAME" = "Country")) %>% 
   select(NAME, LON:CO) 
 tmp <- tmp %>% 
-  mutate(id=1:nrow(tmp))
+  mutate(id = 1:nrow(tmp))   
+  #mutate_at(c("PM2.5", "Household Air Pollution", "Ozone", "NOx", "SO2", "CO", "VOCs"), funs(case_when(. > 7 ~ "High", . > 3 & . <= 7 ~ "Med", . <= 3 ~ "Low")))
 world_spdf@data <- tmp
 
-#create dataframe in a tidy format
+#put dataframe in a tidy format
 tidy_world <- tmp %>%
   gather(pollutant, num_sources, PM2.5:CO)
 
-#create header
+#create dashboard header
 header <- dashboardHeader(
   title = "South and Southeast Asia Air Pollution", titleWidth = "100vw"
 )
 
-#create body
+#create dashboard body
 body <- dashboardBody(
   fluidRow(
     column(width = 8,
            box(width = NULL, solidHeader = TRUE,
-               leafletOutput("mymap", height = "85vh")
-               )
-           ),
+               leafletOutput("mymap", height = "85vh"),
+               box(width = NULL, actionButton("reset_button", "Reset view"), align = "center")
+               ) 
+           ), 
     column(width = 4,
            box(width = NULL,
                selectInput(
@@ -77,7 +80,7 @@ body <- dashboardBody(
                                     text-align: center}"),
                          tags$style("#nodata{text-align:center}")),
                uiOutput("data"))
-           )
+           ) #sidebar
   )
 )
 
@@ -99,31 +102,33 @@ server <- function(input, output) {
 #      geom_point()
 #    
 #  })
+  
   #subset data based on input pollutant
-  df_subset = reactive({
-    a <- subset(tidy_world, pollutant == input$pollutant)
+  df_subset <- reactive({
+    a <- subset(tidy_world, pollutant == input$pollutant) %>% 
+      mutate(num_sources = cut(num_sources, breaks = c(0, 3, 7, 10), labels = c("Low", "Medium", "High")))
   })
   
+  #create leaflet map output
   output$mymap <- renderLeaflet({
-    
-    #create leaflet map
     leaflet(world_spdf) %>% 
       addProviderTiles("Esri.WorldGrayCanvas") %>% 
-      #fitBounds(88, -12, 152, 32)
       setView(lat = 13, lng = 101, zoom = 4) 
   })
   
-  bins <- c(0,2,4,6,8,10)
+  #set bins for color
+  #bins <- c(1,2,4,6,8,10)
   
   observe({
     #set bin and color category
-    pal <- colorBin("YlOrRd", domain = df_subset()$num_sources, bins = bins, na.color = "transparent")
+    #pal <- colorBin("YlOrRd", domain = df_subset()$num_sources, bins = bins, na.color = "transparent")
+    pal <- colorFactor("YlOrRd", df_subset()$num_sources)
     
     #set text popup
-    mytext=paste("Country: ", df_subset()$NAME,"<br/>", "# of Sources: ", df_subset()$num_sources) %>%
+    mytext = paste("Country: ", df_subset()$NAME,"<br/>", "# of Sources: ", df_subset()$num_sources) %>%
       lapply(htmltools::HTML)
     
-    #add data
+    #add data to map
     leafletProxy("mymap", data = world_spdf) %>%
                    clearShapes() %>% 
                    addPolygons(
@@ -148,28 +153,27 @@ server <- function(input, output) {
                      )
     })
   
-  #create legend
+  #create legend separately
   observe({
     proxy <- leafletProxy("mymap", data = df_subset())
     proxy %>% clearControls()
     proxy %>% 
       addLegend(
-        pal <- colorBin("YlOrRd", domain = df_subset()$num_sources, bins = bins, na.color = "transparent"), 
+        pal <- colorFactor("YlOrRd", df_subset()$num_sources),
         values = ~df_subset()$num_sources, 
         opacity = 0.7, 
         title = "Number of sources", 
         position = "bottomleft"
       )
   })
-  #create empty reactive value
+  
   rv <- reactiveValues(data = NULL)
   
   #output plot based on click
   observeEvent(input$mymap_shape_click, {
     event <- input$mymap_shape_click
     
-    #get country name based on ID
-    name <- df_subset()$NAME[df_subset()$id == event$id]
+    name <- df_subset()$NAME[df_subset()$id == event$id] #get country name based on ID
     
     #filter data based on country and pollutant
     rv$tb <- pm %>% 
@@ -199,13 +203,19 @@ server <- function(input, output) {
     
   })
   
-  #clear plot if input changed
+  #clear plot if input changes
   observeEvent(input$pollutant, {
     change <- input$pollutant
     output$plot <- NULL
     output$data <- NULL
     output$nodata <- NULL
     output$country <- NULL
+  })
+  
+  #reset view button
+  observe({
+    input$reset_button
+    leafletProxy("mymap") %>% setView(lat = 13, lng = 101, zoom = 4)
   })
 
     #create plot in popup if data available
